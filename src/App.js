c24916825c92d847dd4abfe56bbf4da225221b1f
fulltostack <fulltostack@gmail.com>
2018-12-21 17:49:26 +0530

    Init empty project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>The Pizza Shop</header>
+      </div>
+    );
+  }
+}
+
+export default App;
